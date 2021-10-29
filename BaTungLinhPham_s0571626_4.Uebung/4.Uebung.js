375e70263dade7ccf32a535d458f8acc5526abbd
mango1603 <bptl1603@gmail.com>
2021-10-30 01:39:49 +0200

    u4

---
+++ BaTungLinhPham_s0571626_4.Uebung/4.Uebung.js
@@ -0,0 +1,128 @@
+/** GTAT2 Game Technology & Interactive Systems **/
+/** 3. Übung  **/
+/** Implementation based on 2. Übung-Lösung from Dr.-Ing. V. Naumburger*/
+/************************************************************************************/
+
+var canvasWidth = window.innerWidth;
+var canvasHeight = window.innerHeight;
+
+var s; // scale
+var x, y; // Coordinate
+
+var basicLength = 5; // [m]
+var bodyHeight = 1.8; //  [m]
+var playgroundWidth = basicLength * 23.9 / 16.9; // [m]
+
+var xBall, yBall; // golf ball
+var dBall = 0.1; // ball diameter in [m]
+var ballColor = "#aaaa00"; //ball color
+var ballSpeed = 3.6; // [m/s]
+var moveBall = false;
+
+var newBtnXPos, newBtnYPos;
+var resetBtnXPos, resetBtnYPos;
+
+const fps = 60;
+var t; // frame duration
+
+function setup() { /* prepare program */
+    frameRate(fps);
+    createCanvas(windowWidth, windowHeight);
+    evaluateConstants();
+
+    s = 0.85 * canvasWidth / playgroundWidth
+    x = 25.0 * canvasWidth / 29.7;
+    y = 15.3 * canvasHeight / 21.0;
+
+    newBtnXPos = 80 * gridX;
+    newBtnYPos = 90 * gridY;
+
+    resetBtnXPos = 10 * gridX;
+    resetBtnYPos = 90 * gridY;
+
+    t = 1 / fps;
+
+    resetBallState();
+}
+
+function draw() {
+    /* administration */
+    background(200);
+
+    // Buttons
+    // NEW-Button
+    push();
+    fill('#00ff00');
+    textAlign(CENTER, CENTER);
+    textSize(2.0 * fontSize);
+    rect(newBtnXPos, newBtnYPos, buttonWidth, buttonHeight);
+    fill(0);
+    text("NEW", newBtnXPos + 0.5 * buttonWidth, newBtnYPos + 0.5 * buttonHeight);
+
+    // RESET-Button
+    fill('#ff0000');
+    rect(resetBtnXPos, resetBtnYPos, buttonWidth, buttonHeight);
+    fill(0);
+    text("RESET", resetBtnXPos + 0.5 * buttonWidth, resetBtnYPos + 0.5 * buttonHeight);
+    pop();
+
+    /* calculation */
+
+    /* display */
+
+    //Playground
+    push();
+    translate(x, y);
+    scale(1, -1);
+    playGround();
+
+    //Golf ball
+    fill(ballColor);
+    shotBall();
+    ellipse(xBall * s, yBall * s, dBall * s);
+
+    //Zero-point marker
+    push();
+    stroke(0);
+    strokeWeight(2);
+    line(10, 0, -10, 0);
+    line(0, 10, 0, -10);
+    pop();
+
+    pop();
+
+    //tips
+    // push();
+    // translate(xio, yio);
+    // push();
+    // translate(x0, y0);
+    // rotate(sinB);
+    // ellipse();
+    // ellipse();
+    // pop();
+    // // pop();
+    //s= 
+    // xBall = s * cosB;
+    // yBall = s * sinB;
+}
+
+function windowResized() { /* responsive design */
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseClicked() {
+    if (mouseX > newBtnXPos &&
+        mouseX < newBtnXPos + buttonWidth &&
+        mouseY > newBtnYPos &&
+        mouseY < newBtnYPos + buttonHeight) {
+        moveBall = true;
+    }
+    if (mouseX > resetBtnXPos &&
+        mouseX < resetBtnXPos + buttonWidth &&
+        mouseY > resetBtnYPos &&
+        mouseY < resetBtnYPos + buttonHeight) {
+        resetBallState();
+    }
+}
---
+++ BaTungLinhPham_s0571626_4.Uebung/addons.js
@@ -0,0 +1,96 @@
+/** GTAT2 Game Technology & Interactive Systems **/
+/** 3. Übung  **/
+/** Implementation based on 2. Übung-Lösung from Dr.-Ing. V. Naumburger*/
+/************************************************************************************/
+
+/* Playground Point Collection*/
+var pgPoints = [
+    [0.82, -0.3], // 0 
+    [0.82, 0], // 1 
+    [-1.2, 0], // 2
+    [-2.1, 0.4], // 3
+    [-3.0, 0], // 4
+    [-3.4, 0], // 5
+    [-3.43, -0.2], // 6
+    [-4.0, -0.2], // 7
+    [-4.03, 0], // 8
+    [-4.9, 0], // 9
+    [-4.9, -0.2], // 10
+    [-5.1, -0.2], // 11
+    [-5.1, 0], // 12
+    [-5.7, 0], // 13
+    [-6.56, 0.9], // 14
+    [-6.56, -0.3], // 15			
+];
+
+var fontSize;
+
+var normPixel, normPixelX, normPixelY;
+var grid, gridX, gridY;
+var buttonWidth, buttonHeight;
+
+function evaluateConstants() {
+    normPixelX = width / 1000.0;
+    normPixelY = height / 1000.0;
+    normPixel = sqrt(normPixelX * normPixelY);
+    fontSize = 12 * normPixel;
+
+    /* Raster 100 x100 */
+    gridX = width / 100.0;
+    gridY = height / 100.0;
+    grid = sqrt(gridX * gridY);
+    buttonWidth = 9 * gridX;
+    buttonHeight = 6 * grid;
+}
+
+function playGround() {
+    //Water hole
+    fill(0, 0, 200);
+    rect(pgPoints[8][0] * s, pgPoints[7][1] * s, (pgPoints[5][0] - pgPoints[8][0]) * s, 0.9 * (pgPoints[5][1] - pgPoints[6][1]) * s);
+
+    // Playground
+    fill(100, 50, 0);
+    beginShape();
+    for (var i = 0; i < pgPoints.length; i++)
+        vertex(pgPoints[i][0] * s, pgPoints[i][1] * s);
+    endShape();
+
+    //Pitch
+    push();
+    strokeWeight(5);
+    noFill();
+    push();
+    stroke(0, 200, 0);
+    beginShape();
+    for (var i = 1; i < 6; i++)
+        vertex(pgPoints[i][0] * s, pgPoints[i][1] * s);
+    endShape();
+    line(pgPoints[8][0] * s, pgPoints[8][1] * s, pgPoints[9][0] * s, pgPoints[9][1] * s);
+    pop();
+
+    //Sand
+    push();
+    stroke(200, 200, 0);
+    beginShape();
+    for (var i = 12; i < 15; i++)
+        vertex(pgPoints[i][0] * s, pgPoints[i][1] * s);
+    endShape();
+    pop();
+    pop();
+}
+
+function resetBallState() {
+    xBall = 0;
+    yBall = dBall / 2;
+    moveBall = false;
+}
+
+function shotBall() {
+    if (moveBall && xBall >= pgPoints[2][0]) {
+        xBall = xBall - t * ballSpeed;
+    }
+
+    if (xBall == pgPoints[2][0]) {
+        moveBall = false;
+    }
+}
